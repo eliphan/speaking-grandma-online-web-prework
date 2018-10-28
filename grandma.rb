555ecacaa1b06c5e6bc91c970a8fea26e90da24e
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-28 00:47:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,12 +14,11 @@
 
  
 def speak_to_grandma (phrase)
-  phrase2 = "I LOVE YOU GRANDMA!"
   
   if phrase == phrase.upcase()
     return "NO, NOT SINCE 1938!"
    
-  elsif phrase == phrase2
+  elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     
   else 
