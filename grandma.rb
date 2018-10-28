f8af327745d89f709817a77ff1b1bf2db0c5de27
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-28 00:40:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-phraselov = "I LOVE YOU GRANDMA"
 def speak_to_grandma (phrase)
   if phrase == phrase.upcase()
     return "NO, NOT SINCE 1938!"
     
-  elsif phrase == phraselov
+  elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     
   else 
